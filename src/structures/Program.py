f70cde86c84a1060af0d338f594e8b6f6b1e6d3b
Mulekotd <joaopedro.gama0675@gmail.com>
2025-11-16 23:55:43 -0300

    docs: Improve README.md file

--- src/structures/Program.py
+++ src/structures/Program.py
@@ -15,8 +15,8 @@ ENTITY_MAP = {
 }
 
 ACTIONS_MAP = {
-    1: "read",
-    2: "visualize",
+    1: "list",
+    2: "read",
     3: "create",
     4: "update",
     5: "delete",
@@ -41,10 +41,10 @@ class Program:
 
         # output paths
         self.output_paths = {
-            "bets": "outputs/bets.txt",
-            "clients": "outputs/clients.txt",
-            "games": "outputs/games.txt",
-            "movimentations": "outputs/movimentations.txt",
+            "bets": "output/bets.txt",
+            "clients": "output/clients.txt",
+            "games": "output/games.txt",
+            "movimentations": "output/movimentations.txt",
         }
 
         # metadata
@@ -162,8 +162,8 @@ class Program:
         entity_list = getattr(self, entity_name)
 
         handler_map = {
-            "read": self.handle_read,
-            "visualize": self.handle_visualize,
+            "list":   self.handle_list,
+            "read":   self.handle_read,
             "create": self.handle_create,
             "update": self.handle_update,
             "delete": self.handle_delete,
@@ -178,8 +178,8 @@ class Program:
 
     # ========== CRUD IMPLEMENTATIONS ==========
 
-    # ------- READ -------
-    def handle_read(self, entity_list: list):
+    # ------- LIST -------
+    def handle_list(self, entity_list: list):
         if not entity_list:
             print("Nenhum registro encontrado.\n")
             return
@@ -193,8 +193,8 @@ class Program:
             if input().lower().strip() == "q":
                 break
 
-    # ------- VISUALIZE -------
-    def handle_visualize(self, entity_list: list):
+    # ------- READ -------
+    def handle_read(self, entity_list: list):
         pk = int(input("Digite a chave primária: ").strip())
 
         found = next((e for e in entity_list if e.get_id() == pk), None)
